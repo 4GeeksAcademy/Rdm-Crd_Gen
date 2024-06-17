1af561c13e008b05b381043fc0d73350cf148dea
mecrophagus <122439515+mecrophagus@users.noreply.github.com>
2024-06-17 16:22:50 +0000

    agregado parte del css

--- src/app.js
+++ src/app.js
@@ -6,6 +6,47 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let switchCardButton = document.querySelector("#button");
+  switchCardButton.addEventListener("click", function() {
+    let suits = ["♦", "♥", "♠", "♣"];
+    let numbers = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "K",
+      "Q",
+      "J",
+      "A"
+    ];
+
+    let randomSuit = Math.floor(Math.random() * suits.length);
+    let randomNumber = Math.floor(Math.random() * numbers.length);
+
+    function colorSuits(icon) {
+      if (icon === "♠" || icon === "♣") {
+        return "black";
+      } else {
+        return "red";
+      }
+    }
+    let cardHTML = `
+    <span id="top-suit" class="simbolo ${colorSuits(suits[randomSuit])}">${
+      suits[randomSuit]
+    }</span>
+      <span class="number">
+        ${numbers[randomNumber]}
+    </span>
+    <span id="bottom-suit" class="simbolo ${colorSuits(suits[randomSuit])}">${
+      suits[randomSuit]
+    }</span`;
+
+    let cardContainer = document.querySelector("#card");
+    cardContainer.innerHTML = cardHTML;
+  });
 };
